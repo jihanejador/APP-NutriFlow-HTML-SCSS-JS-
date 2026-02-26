d42b1c6deb9ac4777feaff2c93414a37b8c9f992
jihanejador <jihanejador@gmail.com>
2026-02-26 12:47:31 +0000

    call function to search in main

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
+import { getAllRecipes, searchRecipes } from './api/recipeProvider.js';
+import { renderLayout, renderRecipes } from './ui/render.js';
 
+async function init() {
+    renderLayout('app');
+    
+    const initialData = await getAllRecipes();
+    renderRecipes(initialData);
+
+    
+    const searchInput = document.getElementById('search-input');
+    
+    searchInput.addEventListener('input', async (e) => {
+        const query = e.target.value;
+        if (query.length > 0) {
+            const filtered = await searchRecipes(query);
+            renderRecipes(filtered);
+        } else {
+            renderRecipes(initialData);
+        }
+    });
+}
+
+init();
